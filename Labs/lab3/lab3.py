13e84053f687ef93065a3f5db527a63348b29535
michael hage <mhage222@gmail.com>
2019-11-04 23:36:30 -0500

    Done, AHE could be fixed

--- Labs/lab3/lab3.py
+++ Labs/lab3/lab3.py
@@ -27,8 +27,8 @@ bf.display_multiple_images([img1, out_img1])
 bf.display_multiple_images([img2, out_img2])
 
 # Adaptive Histogram Equalize
-out_img3 = fun.adaptive_histogram(img1, 7, 7)
-out_img4 = fun.adaptive_histogram(img2, 7, 7)
+out_img3 = fun.adaptive_histogram(img1, 16, 16)
+out_img4 = fun.adaptive_histogram(img2, 16, 16)
 
 bf.display_multiple_images([img1, out_img3])
 bf.display_multiple_images([img2, out_img4])
@@ -39,7 +39,7 @@ img3 = cv2.imread(r"Images\testing\sharpen\7.2.01-small.png", 0)
 img4 = cv2.imread(r"Images\testing\sharpen\digital_orca_blurred.png", 0)
 
 r = 2
-k = 0.2
+k = 1
 
 # Unsharpen Mask
 
@@ -51,11 +51,61 @@ bf.display_multiple_images([img4, out_img6])
 
 # Laplacian Sharpen
 
+k = 1
+
 out_img7 = fun.laplacian_sharpen(img3, k)
 out_img8 = fun.laplacian_sharpen(img4, k)
 
 bf.display_multiple_images([img3, out_img7])
 bf.display_multiple_images([img4, out_img8])
 
-# CLAHE 
 
+# Section 2.3
+
+img5 = cv2.imread(r"Images\enhance\noise_additive.png", 0)
+img6 = cv2.imread(r"Images\enhance\noise_multiplicative.png", 0)
+img7 = cv2.imread(r"Images\enhance\noise_impulsive.png", 0)
+img8 = cv2.imread(r"Images\enhance\snowglobe.png", 0)
+
+# Additive Noise
+
+out_img9 = cv2.medianBlur(img5, 3)
+
+k = 1
+r = 3
+out_img9 = fun.unsharp_mask(out_img9, r, k)
+
+k = 0.4
+out_img9 = fun.laplacian_sharpen(out_img9, k)
+
+bf.display_multiple_images([img5, out_img9])
+
+# Multiplicative Nosie
+
+average = np.array([[1, 1, 1],
+                    [1, 1, 1],
+                    [1, 1, 1]])
+
+out_img10 = np.array(bf.spatial_filter(img6, average / np.sum(average)), np.uint8)
+
+k = 0.1
+out_img10 = fun.laplacian_sharpen(out_img10, k)
+
+bf.display_multiple_images([img6, out_img10])
+
+# Impulsive Noise
+
+out_img11 = cv2.medianBlur(img7, 3)
+
+bf.display_multiple_images([img7, out_img11])
+
+# Snowglobe
+
+r = 2
+gauss = fun.gaussian_kernel(1, r)
+gauss_sum = np.sum(gauss)
+
+out_img12 = np.array(bf.spatial_filter(img8, gauss / gauss_sum), np.uint8)
+out_img12 = fun.histogram_equalization(out_img12)
+
+bf.display_multiple_images([img8, out_img12])
--- Labs/lab3/lab3_functions.py
+++ Labs/lab3/lab3_functions.py
@@ -6,9 +6,9 @@ Created on Tue Oct 22 20:32:55 2019
 """
 
 import numpy as np
-import cv2
 import basic_functions as bf
 
+# Create Gaussian Kernel Size r
 def gaussian_kernel(sigma, r):
     
     size = 2 * r + 1
@@ -27,39 +27,26 @@ def gaussian_kernel(sigma, r):
     
     return np.array(np.round(kernel / kernel[0][0]), np.int)
 
-def laplace_gaussian_kernel(sigma, r):
-    
-    size = 2 * r + 1
-    
-    kernel = np.zeros([size, size])
-    
-    m, m = bf.padding_param(size)
-    
-    k_2 = 2 * sigma**2
-    k_6 = 2 * np.pi * sigma**6
-    
-    for i in range(0, size):
-        for j in range(0, size):
-            
-            kernel[i][j] = ( (i**2 + j**2 - k_2) / k_6) * np.exp( -( (i - m)**2 + (j - m)**2 ) / k_2 )
-    
-    return np.array(np.round(kernel / kernel[0][0]), np.int)
-
+#Apply Global Histogram Equalization
 def histogram_equalization(img):
     
+#    Check for unsigned integer 8-bit
     if img.dtype != np.uint8:
         return img
     
     out_img = img.copy()
     MAX = np.iinfo(np.uint8).max
     
-    his = np.zeros(MAX)
+#    Creates the histogram
+    his = np.zeros(MAX+1)
     
+#    Calculate the histogram
     for i in range(0, len(img)):
         for j in range(0, len(img[i])):
             
             his[img[i][j]] += 1
-            
+     
+#    Calculate the CDF
     cdf = np.zeros_like(his)    
     cdf[0] = his[0]
     
@@ -68,6 +55,7 @@ def histogram_equalization(img):
     
     cdf = cdf / img.size * MAX
     
+#    Apply the CDF
     for i in range(0, len(img)):
         for j in range(0, len(img[i])):
             
@@ -75,10 +63,10 @@ def histogram_equalization(img):
     
     return out_img
 
-
+#Apply Local Histogram Equalization
 def adaptive_histogram(img, H, W):
     
-     
+#    Check for unsigned integer 8-bit
     if img.dtype != np.uint8:
         return img
     
@@ -86,70 +74,82 @@ def adaptive_histogram(img, H, W):
     MAX = np.iinfo(np.uint8).max
     m,n = img.shape
     
-    pdf = np.zeros(MAX)
-    his = np.zeros(MAX)
-    cdf = np.zeros(MAX)
+    pdf = np.zeros(MAX+1)
+    cdf = np.zeros(MAX+1)
     
+#    Zero padding the Image
     col_right, col_left = bf.padding_param(W)
     row_bottom, row_top = bf.padding_param(H)
     
     img_temp = np.array(np.zeros([m+row_top+row_bottom,n+col_left+col_right]), np.uint8)
     img_temp[row_top:m+row_top, col_left:n+col_left] = 1
     img_temp[row_top:m+row_top, col_left:n+col_left] *= img
-
+    
+#    Iterate over sections and perform the local histogram equalize operation
     for i in range(row_top,m+row_top):
         for j in range(col_left,n+col_left):
             
-            cdf.fill(0)
-            his.fill(0)
-            pdf.fill(0)
+#            Reset All Parameters
+            cdf[:] = 0
+            pdf[:] = 0
             
             snap = img_temp[i-row_top: i+row_bottom+1, j-col_left: j+col_right+1].copy()
             for x in range(0,len(snap)):
                 for y in range(0,len(snap[x])):
-
-                    his[snap[x][y]] += 1
-
-            pdf[:] = his[:] / snap.size
-               
+                    
+                    pdf[snap[x][y]] += 1
+        
             cdf[0] = pdf[0]
             
-            for a in range(1,snap[row_top][col_left]+1):
+            for a in range(1,MAX+1):
                 cdf[a] = pdf[a] + cdf[a - 1]
-                
-            out_img[i-row_top][j-col_left] = cdf[snap[row_top][col_left]] * MAX
+            
+            out_img[i-row_top][j-col_left] = cdf[snap[row_top][col_left]] * MAX / snap.size
             
     return out_img
 
+# Unsharpen Image
 def unsharp_mask(img, r, k):
     
     if img.dtype != np.uint8:
         return img
     
-    out_img = img.copy()
+    out_img = np.array(img.copy(), np.double)
     img_temp = img.copy()
     
+#    Make gaussian kernel
     gauss = gaussian_kernel(1, r)
     gauss_sum = np.sum(gauss)
     
+#    Apply gaussian kernel
     img_blur = bf.spatial_filter(img, gauss / gauss_sum)
     
-    out_img = img_temp + k * (img_temp - img_blur)
+#    Perform unsharp operation
+    out_img = img_temp - k * (img_temp - img_blur)
     
-    return np.array(np.round(out_img), np.uint8)
+    return np.array(out_img, np.uint8)
 
+#Sharpen an Image using Laplacian Kernel
 def laplacian_sharpen(img, k):
     
-    out_img = img.copy()
+    out_img = np.array(img.copy(), np.double)
     img_temp = img.copy()
     
+#    Laplace kernel
     laplace = np.array([[-1, -1, -1],
                         [-1, 8, -1],
                         [-1, -1, -1]])
     
-#    laplace = laplace_gaussian_kernel(1, 1)
+#    Apply kernel and operation
     img_lpc = bf.spatial_filter(img, laplace)
+    out_img = img_temp + k * img_lpc
     
-    out_img = img_temp - k * img_lpc
+    for i in range(0, len(out_img)):
+        for j in range(0, len(out_img[i])):
+            
+            if out_img[i][j] < 0:
+                out_img[i][j] = 0
+            elif out_img[i][j] > 255:
+                out_img[i][j] = 255
     
     return np.array(out_img, np.uint8)
